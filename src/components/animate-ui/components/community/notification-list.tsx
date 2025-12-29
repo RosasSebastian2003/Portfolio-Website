21d8f05ff6087df9c077d26c1d89f26cc447830e
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2025-12-29 04:08:42 -0600

    Fix: se agrego un estado fijo para dispositivos pequeños en la seccion de redes sociales

--- src/components/animate-ui/components/community/notification-list.tsx
+++ src/components/animate-ui/components/community/notification-list.tsx
@@ -2,6 +2,7 @@
 
 import { ArrowUpRight } from 'lucide-react';
 import { motion, type Transition } from 'motion/react';
+import { useMediaQuery } from 'react-responsive';
 
 const notifications = [
     {
@@ -60,8 +61,69 @@ const viewAllTextVariants = {
 };
 
 function NotificationList() {
+  // Detectar si es móvil o tablet (< 1024px)
+  const isMobileOrTablet = useMediaQuery({ maxWidth: 1024 });
+
   return (
-    <motion.div
+    <>
+    {isMobileOrTablet ? (
+      <motion.div
+      className="bg-neutral-900 p-3 rounded-3xl w-xs space-y-3 shadow-md shadow-white-500/10"
+      initial="expanded"
+      whileHover="expanded"
+    >
+      <div>
+        {notifications.map((notification, i) => (
+          <a href={notification.url} target="_blank" rel="noopener noreferrer">
+            <motion.div
+              key={notification.id}
+              className="bg-neutral-800 rounded-xl px-4 py-2 shadow-sm shadow-white-500/20 hover:shadow-lg transition-shadow duration-200 relative"
+              variants={getCardVariants(i)}
+              transition={transition}
+              style={{
+                zIndex: notifications.length - i,
+              }}
+            >
+              <div className='flex items-center'>
+                <img src={notification.icon} alt="" className='w-6 h-6 mr-2' />
+                <div>
+                  <div className="flex justify-between items-center">
+                    <h1 className="text-sm font-medium text-white">{notification.title}</h1>
+                  </div>
+                  <div className="text-xs text-white-700 font-medium">
+                    <span>{notification.subtitle}</span>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </a>
+        ))}
+      </div>
+
+      <div className="flex items-center gap-2">
+        <div className="size-5 rounded-full bg-white text-neutral text-xs flex items-center justify-center font-medium">
+          {notifications.length}
+        </div>
+        <span className="grid">
+          <motion.span
+            className="text-sm font-medium text-neutral-300 row-start-1 col-start-1"
+            variants={notificationTextVariants}
+            transition={textSwitchTransition}
+          >
+            My Social Media
+          </motion.span>
+          <motion.span
+            className="text-sm font-medium text-neutral-300 flex items-center gap-1 cursor-pointer select-none row-start-1 col-start-1"
+            variants={viewAllTextVariants}
+            transition={textSwitchTransition}
+          >
+            View all <ArrowUpRight className="size-4" />
+          </motion.span>
+        </span>
+      </div>
+    </motion.div>
+    ) : (
+      <motion.div
       className="bg-neutral-900 p-3 rounded-3xl w-xs space-y-3 shadow-md shadow-white-500/10"
       initial="collapsed"
       whileHover="expanded"
@@ -116,6 +178,8 @@ function NotificationList() {
         </span>
       </div>
     </motion.div>
+    )}
+    </>
   );
 }
 
